9c22a902dd9b0a32f784e7b4d1a6ae963a9514cf
ELVIS <ELVIS@LAP-ED>
2023-12-01 06:46:13 -0500

    Agregar archivos de proyecto.

---
+++ Clase-Poo/Mascota.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_Poo
+{
+    public class Mascota
+    {
+        //Variable
+        public string _Id { get; set; }
+        public string _Nombre { get; set; }
+        public int _Edad {  get; set; }
+
+        // constructor
+        public Mascota(string Id, string Nombre, int Edad) 
+        {
+            _Nombre = Nombre;
+            _Edad = Edad;
+            _Id = Id;
+        }
+        //METODO
+        public void Caminar () 
+        {
+            Console.WriteLine("Estoy caminando");        
+        }
+        //Sobreescribir un método
+        public void CaminarDos() 
+        {
+            Console.WriteLine("Estoy caminando" + "arriba");
+        }
+    }
+}
---
+++ Clase-Poo/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//PERSONA
+
+//MODIFICADORES DE ACCESO
+//public, para que cuando se creen otras clases pueden tomar el valor de la variable, por ejemplo en "public string Nombre"
+//private, para que cuando se creen otras clases no puedan tomar ese valor de la variable
+//protected, para que cuando se creen otras clases no pueden tomar sus valores
+
+
+string Nombre; //"string" es para texto//
+int Edad; //"int" es para números enteros//
+decimal Salarios; //"decimal" es para números decimales, por ejemplo 10.5//
